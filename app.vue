1a9f61f778a2dfa99e91af31f6cc21ad95ace94b
Mattz56 <Matthieu.audo.56@gmail.com>
2025-02-14 08:28:15 -0500

    First commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <UFormGroup label="Email">
+      <UInput placeholder="you@example.com" icon="i-heroicons-envelope" />
+    </UFormGroup>
+    <h1 class="text-red-500 font-bold underline">PORTOFLIO V2</h1>
+  </div>
+</template>
